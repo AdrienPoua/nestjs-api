056dd6de8fad7dc923448ba3d1eba05eb3e6630c
zartax95 <adrienvilb@msn.com>
2024-09-05 18:58:38 +0200

    add users controllers & specs

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
+
+  const mockUser = { username: 'John Doe', password: 'password', id: '1' };
+  const mockReq = { user: { username: 'John Doe' } };
+  const mockUpdatedUser = {
+    username: 'UpdatedUser',
+    password: 'updatedPassword',
+  };
+  const mockUpdatedUserWithId = {
+    ...mockUpdatedUser,
+    id: '1',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            findOne: jest.fn(),
+            update: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({
+        canActivate: jest.fn(() => true), // Mock JwtAuthGuard pour autoriser l'accès
+      })
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('test routes', () => {
+    it('should return a user when authenticated', async () => {
+      jest.spyOn(usersService, 'findOne').mockResolvedValue(mockUser);
+      const result = await controller.get(mockReq);
+      expect(usersService.findOne).toHaveBeenCalledWith(mockUser.username);
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should return the updated user when authenticated', async () => {
+      jest
+        .spyOn(usersService, 'update')
+        .mockResolvedValue(mockUpdatedUserWithId);
+      const result = await controller.update(mockReq, mockUpdatedUser);
+      expect(result).toEqual({
+        id: expect.any(String),
+        username: mockUpdatedUser.username,
+        password: expect.any(String),
+      });
+      expect(usersService.update).toHaveBeenCalledWith(
+        mockUser.username,
+        mockUpdatedUser,
+      );
+    });
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Patch,
+  Body,
+  Request,
+  UseGuards,
+  Get,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async get(@Request() req: { user: { username: string } }) {
+    return this.usersService.findOne(req.user.username);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch()
+  async update(
+    @Request() req: { user: { username: string } },
+    @Body() body: { username?: string; password?: string },
+  ) {
+    return this.usersService.update(req.user.username, body);
+  }
+}
