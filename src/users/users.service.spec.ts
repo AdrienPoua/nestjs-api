4b844396b26333b36d464bfb3c2580324b455fb0
zartax95 <adrienvilb@msn.com>
2024-09-05 17:31:24 +0200

    feat : users services with spec

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { NotFoundException, BadRequestException } from '@nestjs/common';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -8,11 +9,112 @@ describe('UsersService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService],
     }).compile();
-
     service = module.get<UsersService>(UsersService);
   });
 
+  /******************************************
+   *        SECTION : Initialize data       *
+   ******************************************/
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  const user = {
+    id: '123',
+    username: 'John Doe',
+    password: 'password',
+  };
+
+  /******************************************
+   *        SECTION : Errors handling       *
+   ******************************************/
+
+  describe('Test errors cases', () => {
+    it('should throw NotFoundException if user is not found', async () => {
+      try {
+        await service.findOne('non_existing_user');
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
+
+    it('should throw NotFoundException if updating a non-existing user', async () => {
+      const updatedUser = {
+        username: 'Jeanne',
+        password: '1234',
+      };
+
+      try {
+        await service.update('invalid_username', updatedUser);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
+    it('should throw BadRequestException if updating with invalid parameters', async () => {
+      const updatedUser = {
+        username: 'Jeanne',
+        invalid: 'invalid', // Paramètre non autorisé
+      };
+
+      await service.create(user);
+
+      try {
+        await service.update(user.username, updatedUser);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+      }
+    });
+  });
+
+  /******************************************
+   *        SECTION : create               *
+   ******************************************/
+
+  describe('User creation', () => {
+    it('should create a new user and return it with an id and without password', async () => {
+      const result = await service.create(user);
+      expect(result).toEqual({
+        username: user.username,
+        id: expect.any(String),
+      });
+    });
+  });
+
+  /******************************************
+   *        SECTION : findOne               *
+   ******************************************/
+
+  describe('User search', () => {
+    it('should find a user by username and return it with an id', async () => {
+      await service.create(user);
+      const result = await service.findOne(user.username);
+      expect(result).toEqual({
+        username: user.username,
+        id: expect.any(String),
+        password: expect.any(String),
+      });
+    });
+  });
+
+  /******************************************
+   *        SECTION : update               *
+   ******************************************/
+
+  describe('User update', () => {
+    it('should update a user and return it with the updated parameters', async () => {
+      const updatedUser = {
+        username: 'Jeanne',
+        password: '1234',
+      };
+
+      // Ajout de l'utilisateur avant la mise à jour
+      await service.create(user);
+
+      const result = await service.update(user.username, updatedUser);
+      expect(result).toEqual({
+        username: updatedUser.username,
+        password: expect.any(String),
+        id: expect.any(String),
+      });
+    });
   });
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
+export interface User {
+  id: string;
+  username: string;
+}
+
+export interface UserWithPassword extends User {
+  password: string;
+}
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: UserWithPassword[] = [];
+
+  async create(userData: {
+    username: string;
+    password: string;
+  }): Promise<User> {
+    const hashedPassword: string = await bcrypt.hash(userData.password, 10);
+    const user: UserWithPassword = {
+      id: uuidv4(),
+      username: userData.username,
+      password: hashedPassword,
+    };
+    this.users.push(user);
+
+    // Retourne l'utilisateur sans son mot de passe
+    return { id: user.id, username: user.username };
+  }
+
+  async findOne(username: string): Promise<UserWithPassword> {
+    const user = this.users.find((user) => user.username === username);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async update(
+    username: string,
+    updateData: { username?: string; password?: string },
+  ): Promise<UserWithPassword> {
+    const user = await this.findOne(username);
+    if (user) {
+      if (updateData.password) {
+        // Si un mot de passe est fourni, le hacher et le mettre à jour
+        updateData.password = await bcrypt.hash(updateData.password, 10);
+      }
+      // Mettre à jour l'utilisateur
+      if (updateData.username) {
+        user.username = updateData.username;
+      }
+      // Retourner l'utilisateur mis à jour
+      return user;
+    }
+    throw new NotFoundException('User not found');
+  }
+}
